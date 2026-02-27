a41b0aa5660c328db177b927ea0fa6d24f140db5
EvertonAbreu <everton.comshowl@gmail.com.com>
2026-02-27 15:28:37 -0300

    fix: remove superuser from settings

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -179,14 +179,3 @@ if not DEBUG:
 
 
 
-if os.environ.get("DJANGO_SUPERUSER_PASSWORD"):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-
-    if username and password:
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username, email, password)
--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Cria superusuário automaticamente
+# Cria superusuário se não existir
 python manage.py createsuperuser \
-    --noinput \
-    --username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL || true
+  --noinput \
+  --username $DJANGO_SUPERUSER_USERNAME \
+  --email $DJANGO_SUPERUSER_EMAIL || true
